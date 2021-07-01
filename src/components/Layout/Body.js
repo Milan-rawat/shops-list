149ced8b1aec495fdfefc6fd1f8c387136088bfa
Milan Rawat <milanrawat086@gmail.com>
2021-07-01 10:30:24 +0530

    area and type filter added

--- src/components/Layout/Body.js
+++ src/components/Layout/Body.js
@@ -8,6 +8,8 @@ import { shopListActions } from "../../store/index";
 import Modal from "../Modal/Modal";
 
 const Body = () => {
+  const [areaFilter, setAreaFilter] = useState("All");
+  const [typeFilter, setTypeFilter] = useState("All");
   const [showModal, setShowModal] = useState(false);
   const [deletingShop, setDeletingShop] = useState({
     id: null,
@@ -15,6 +17,8 @@ const Body = () => {
   });
 
   const shopsList = useSelector((state) => state.shopsList.shopsList);
+
+  const [allShops, setAllShops] = useState(shopsList);
 
   const dispatch = useDispatch();
 
@@ -35,11 +39,40 @@ const Body = () => {
     setShowModal(false);
   };
 
+  const applyFilterHandler = () => {
+    console.log(areaFilter, typeFilter);
+    if (areaFilter === "All" && typeFilter === "All") {
+      setAllShops(shopsList);
+    }
+    if (areaFilter !== "All" && typeFilter !== "All") {
+      setAllShops(() => {
+        const list = shopsList.filter(
+          (shop) => shop.shopArea === areaFilter && shop.shopType === typeFilter
+        );
+        return list;
+      });
+    }
+    if (areaFilter !== "All" && typeFilter === "All") {
+      setAllShops(() => {
+        const list = shopsList.filter((shop) => shop.shopArea === areaFilter);
+        if (areaFilter === "All") return shopsList;
+        else return list;
+      });
+    }
+    if (areaFilter === "All" && typeFilter !== "All") {
+      setAllShops(() => {
+        const list = shopsList.filter((shop) => shop.shopType === typeFilter);
+        if (typeFilter === "All") return shopsList;
+        else return list;
+      });
+    }
+  };
+
   let ALLSHOPS = [];
   if (shopsList.length === 0) {
     ALLSHOPS = <h3 style={{ textAlign: "center" }}>No Shops available.</h3>;
   } else {
-    ALLSHOPS = shopsList.map((shop) => (
+    ALLSHOPS = allShops.map((shop) => (
       <div className={classes.shop} key={shop.id}>
         <div className={classes.shopSection}>
           <label htmlFor="shopTitle">Shop Name</label>
@@ -74,6 +107,49 @@ const Body = () => {
           onConfirmDelete={(shopId) => removeShopHandler(shopId)}
         />
       )}
+      <div className={classes.filters}>
+        <p>Filters: -</p>
+        <div className={`${classes.filter} ${classes.areaFilter}`}>
+          <label htmlFor="areaFilter"> Shop Area</label>
+          <select
+            id="areaFilter"
+            // className={classes.selectInput}
+            value={areaFilter}
+            onChange={(e) => setAreaFilter(e.target.value)}
+            required
+          >
+            <option value="All">All</option>
+            <option>Thane</option>
+            <option>Pune</option>
+            <option>Mumbai</option>
+            <option>Suburban</option>
+            <option>Nashik</option>
+            <option>Nagpur</option>
+            <option>Ahmednagar</option>
+            <option>Solapur</option>
+            <option>Najibabad</option>
+            <option>Kotdwara</option>
+          </select>
+        </div>
+        <div className={`${classes.filter} ${classes.typeFilter}`}>
+          <label htmlFor="typeFilter">Shop Category</label>
+          <select
+            id="typeFilter"
+            className={classes.selectInput}
+            value={typeFilter}
+            onChange={(e) => setTypeFilter(e.target.value)}
+            required
+          >
+            <option value="All">All</option>
+            <option>Grocery</option>
+            <option>Butcher</option>
+            <option>Baker</option>
+            <option>Chemist</option>
+            <option>Stationery shop</option>
+          </select>
+        </div>
+        <button onClick={applyFilterHandler}>Apply filters</button>
+      </div>
       <div className={classes.shopLists}>{ALLSHOPS}</div>
     </div>
   );
