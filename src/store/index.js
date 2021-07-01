a548491986b505c0de6eaeeebdaa7e8656154179
Milan Rawat <milanrawat086@gmail.com>
2021-07-01 10:31:11 +0530

    dummy default data removed

--- src/store/index.js
+++ src/store/index.js
@@ -1,36 +1,9 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const shopsList = [
-  {
-    id: "1",
-    shopName: "Kanha groceries",
-    shopArea: "Thane",
-    shopType: "Grocery",
-    openingDate: "05-05-2020",
-    closingDate: "29-06-2021",
-  },
-  {
-    id: "2",
-    shopName: "Kanha groceries",
-    shopArea: "Thane",
-    shopType: "Grocery",
-    openingDate: "05-05-2020",
-    closingDate: "29-06-2021",
-  },
-  {
-    id: "3",
-    shopName: "Kanha Baker",
-    shopArea: "Thane",
-    shopType: "Baker",
-    openingDate: "05-05-2020",
-    closingDate: "29-06-2021",
-  },
-];
-
 const shopListSlice = createSlice({
   name: "shopList",
   initialState: {
-    shopsList: shopsList,
+    shopsList: [],
   },
   reducers: {
     addShop(state, action) {
